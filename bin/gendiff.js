a924edbbc3d45fa14da6266607f553db008f8d49
dreamer <bambukovnikolaj@gmail.com>
2023-02-15 13:11:30 +0500

    исправление ошибок eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,12 +4,12 @@ import genDiff from '../src/index.js';
 
 const program = new Command();
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .option('-f, --format <type>', 'output format')
-    .action((filepath1, filepath2) => {
-      console.log(genDiff(filepath1, filepath2));
-    });
-    program.parse();
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
+program.parse();
