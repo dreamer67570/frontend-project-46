04358ca3092a027b72344fcb9a8f02fe29ad4b62
dreamer <bambukovnikolaj@gmail.com>
2023-02-15 12:39:56 +0500

    добавил action() для вызова библиотеки

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 program
@@ -7,5 +8,8 @@ program
     .version('1.0.0')
     .argument('<filepath1>')
     .argument('<filepath2>')
-    .option('-f, --format <type>', 'output format');
-program.parse();
+    .option('-f, --format <type>', 'output format')
+    .action((filepath1, filepath2) => {
+      console.log(genDiff(filepath1, filepath2));
+    });
+    program.parse();
