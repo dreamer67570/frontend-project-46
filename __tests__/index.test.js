22d4b8072def88f42fd06152e4063636272f1bde
dreamer <bambukovnikolaj@gmail.com>
2023-02-15 17:05:46 +0500

    добавил тесты для YAML

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,5 +13,12 @@ test('check diff flat JSON files', () => {
   const fileName1 = getFixturePath('file1.json');
   const fileName2 = getFixturePath('file2.json');
 
-  expect(genDiff(fileName1, fileName2)).toEqual(readFile('expect.txt'));
+  expect(genDiff(fileName1, fileName2)).toEqual(readFile('test_JSON.txt'));
+});
+
+test('check diff flat YAML files', () => {
+  const fileName1 = getFixturePath('file1.yml');
+  const fileName2 = getFixturePath('file2.yml');
+
+  expect(genDiff(fileName1, fileName2)).toEqual(readFile('test_YAML.txt'));
 });
