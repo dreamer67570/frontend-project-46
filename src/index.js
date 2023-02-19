ce0ee8d9fecbcd096ab1d26f5edc2bfafa9c8ae3
dreamer <bambukovnikolaj@gmail.com>
2023-02-19 14:30:37 +0500

    убрал функцию работы с плоскиим элементами

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
+import buildTree from './buildTree.js';
+import stylish from './stylish.js';
 
 const getRelativePath = (filename) => path.resolve(filename);
 const readFile = (filePatch) => fs.readFileSync(getRelativePath(filePatch), 'utf8');
@@ -15,25 +16,6 @@ export default (filePatch1, filePatch2) => {
   const obj1 = parse(file1, getFormat(filePatch1));
   const obj2 = parse(file2, getFormat(filePatch1));
 
-  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
-  const sortKeys = _.sortBy(keys);
-
-  let result = '{\n';
-  sortKeys.map((key) => {
-    if (!_.has(obj1, key)) {
-      result += `  + ${key}: ${obj2[key]}\n`;
-    } else if (!_.has(obj2, key)) {
-      result += `  - ${key}: ${obj1[key]}\n`;
-    } else if (obj1[key] !== obj2[key]) {
-      result += `  - ${key}: ${obj1[key]}\n`;
-      result += `  + ${key}: ${obj2[key]}\n`;
-    } else {
-      result += `    ${key}: ${obj1[key]}\n`;
-    }
-    return 0;
-  });
-
-  result += '}';
-
-  return result;
+  const treeDiff = buildTree(obj1, obj2);
+  return stylish(treeDiff);
 };
