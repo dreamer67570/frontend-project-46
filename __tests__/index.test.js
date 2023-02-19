5cffad32c3a08afd0a3982d24c541b49467a3492
dreamer <bambukovnikolaj@gmail.com>
2023-02-19 15:00:26 +0500

    обновил тесты

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,12 +13,12 @@ test('check diff flat JSON files', () => {
   const fileName1 = getFixturePath('file1.json');
   const fileName2 = getFixturePath('file2.json');
 
-  expect(genDiff(fileName1, fileName2)).toEqual(readFile('test_JSON.txt'));
+  expect(genDiff(fileName1, fileName2)).toEqual(readFile('test.txt'));
 });
 
 test('check diff flat YAML files', () => {
   const fileName1 = getFixturePath('file1.yml');
   const fileName2 = getFixturePath('file2.yml');
 
-  expect(genDiff(fileName1, fileName2)).toEqual(readFile('test_YAML.txt'));
+  expect(genDiff(fileName1, fileName2)).toEqual(readFile('test.txt'));
 });
