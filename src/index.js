0c7d5b5a517b417e8fd0ef85cf5a0d083c12bac6
dreamer <bambukovnikolaj@gmail.com>
2023-02-20 18:30:18 +0500

    добавил переменную для формата вывода результата

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
 import buildTree from './buildTree.js';
-import stylish from './stylish.js';
+import formatter from './formatters/index.js';
 
 const getRelativePath = (filename) => path.resolve(filename);
 const readFile = (filePatch) => fs.readFileSync(getRelativePath(filePatch), 'utf8');
 
 const getFormat = (filePatch) => filePatch.split('.')[1];
 
-export default (filePatch1, filePatch2) => {
+export default (filePatch1, filePatch2, printFormat) => {
   const file1 = readFile(filePatch1);
   const file2 = readFile(filePatch2);
 
@@ -17,5 +17,5 @@ export default (filePatch1, filePatch2) => {
   const obj2 = parse(file2, getFormat(filePatch1));
 
   const treeDiff = buildTree(obj1, obj2);
-  return stylish(treeDiff);
+  return formatter(treeDiff, printFormat);
 };
