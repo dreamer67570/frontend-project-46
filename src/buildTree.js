c9d6b8ad732340f9a3708eb3308f1e1df52f9f59
dreamer <bambukovnikolaj@gmail.com>
2023-02-22 21:32:51 +0500

    рефакторинг

--- src/buildTree.js
+++ src/buildTree.js
@@ -2,45 +2,23 @@ import _ from 'lodash';
 
 const buildTree = (obj1, obj2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2));
-  const sortKeys = _.sortBy(keys);
-  const diff = sortKeys.map((key) => {
+  const diff = _.sortBy(keys).map((key) => {
     if (!_.has(obj1, key)) {
-      return {
-        key,
-        value: obj2[key],
-        diffType: 'added',
-      };
+      return { key, value: obj2[key], diffType: 'added' };
     }
     if (!_.has(obj2, key)) {
-      return {
-        key,
-        value: obj1[key],
-        diffType: 'deleted',
-      };
+      return { key, value: obj1[key], diffType: 'deleted' };
     }
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return {
-        key,
-        diffType: 'hasСhildren',
-        children: buildTree(obj1[key], obj2[key]),
-      };
+      return { key, diffType: 'hasСhildren', children: buildTree(obj1[key], obj2[key]) };
     }
 
     if (obj1[key] === obj2[key]) {
-      return {
-        key,
-        value: obj1[key],
-        diffType: 'unchanged',
-      };
+      return { key, value: obj1[key], diffType: 'unchanged' };
     }
 
-    return {
-      key,
-      value: { before: obj1[key], after: obj2[key] },
-      diffType: 'changed',
-    };
+    return { key, value: { before: obj1[key], after: obj2[key] }, diffType: 'changed' };
   });
   return diff;
 };
-
 export default buildTree;
